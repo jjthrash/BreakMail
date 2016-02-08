69e48f02bca2646bd9a4ec451d9b8898caf58156
Jimmy Thrasher <jimmy@jimmythrasher.com>
2016-02-08 10:05:07 -0500

    Add a timestamp to the subject so I can tell which email came through

--- BreakMail/ViewController.m
+++ BreakMail/ViewController.m
@@ -32,7 +32,7 @@
     NSData *fileData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"myPDF.pdf" ofType:nil]];
     self.mailController = [[MFMailComposeViewController alloc] init];
     self.mailController.mailComposeDelegate = self;
-    [self.mailController setSubject:@"Subject"];
+    [self.mailController setSubject:[NSString stringWithFormat:@"Subject %@", [NSDate date]]];
     [self.mailController setMessageBody:@"Body" isHTML:YES];
     [self.mailController addAttachmentData:fileData mimeType:@"application/pdf" fileName:@"photos.pdf"];
     [self presentViewController:self.mailController animated:YES completion:nil];
